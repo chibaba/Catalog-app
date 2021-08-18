3492e65719670548b2cbdfd7a2a78c2c53244765
chi <chinedu.onyeoka@gmail.com>
2021-08-18 14:28:23 +0100

    update item dto

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -31,6 +31,12 @@ namespace Catalog.Repositories
     {
       items.Add(item);
     }
+
+    public void UpdateItem(Item item)
+    {
+      var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+      items[index] = item;
+    }
   }
 
 }
