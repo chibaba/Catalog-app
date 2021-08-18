070ea18f569ab5537f8311049a941b05bade2627
chi <chinedu.onyeoka@gmail.com>
2021-08-18 14:47:04 +0100

    updated item

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -52,5 +52,22 @@ namespace Catalog.Controllers
 
       return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
     }
+
+// PUT /items/{id}
+[HttpPut("{id}")]
+public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto) {
+  var existingItem = repository.GetItem(id);
+  if  (existingItem == null)
+  {
+    return NotFound();
+  }
+  Item UpdatedItem = existingItem with {
+    Name = itemDto.Name,
+    Price = itemDto.Price
+  };
+
+  repository.UpdateItem( UpdatedItem);
+  return NoContent();
+}
   }
 }
